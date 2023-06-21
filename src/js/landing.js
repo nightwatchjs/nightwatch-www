72e5328e06c3d57e11cfde5776c9eeeed60403a1
Binayak Ghosh <ghoshbinayak@gmail.com>
2023-06-21 14:08:04 +0530

    navbar fixes

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,11 +1,17 @@
-docReady(function () {
+import {onRender} from 'postdoc/client';
+
+onRender(() => {
   // Copy Button
   function addCopyEventListeners(buttonSelector, textSelector) {
     const copyButton = document.querySelector(buttonSelector);
 
+    if (!copyButton){
+      return;
+    }
+
     copyButton.addEventListener('click', async function () {
       const copyText = document.querySelector(textSelector);
-
+  
       try {
         await navigator.clipboard.writeText(copyText.textContent);
         copyButton.innerHTML = 'Copied!';
@@ -13,20 +19,20 @@ docReady(function () {
       } catch (err) {
         console.error('Failed to copy text: ', err);
       }
-
+  
       setTimeout(() => {
         copyButton.innerHTML = 'Copy';
         copyButton.classList.remove('active');
       }, 3000);
     });
   }
-
+  
   addCopyEventListeners('.hero__action-copy-command-button', '.hero__action-copy-command-text');
   addCopyEventListeners(
     '.call-to-action-content-action-copy-command-button',
     '.call-to-action-content-action-copy-command-text'
   );
-
+  
   addScript('https://cdn.jsdelivr.net/npm/@docsearch/js@3', 'docsearch-script', function () {
     docsearch({
       //appId: 'H6WO0X38VS',
@@ -44,18 +50,18 @@ docReady(function () {
             item.hierarchy.lvl1 = item.content.replace('https://nightwatchjs.org/', '');
             item.type = 'lvl1';
           }
-
+  
           return item;
         });
       },
       debug: false
     });
   });
-
+  
   const breakpoint = window.matchMedia('(min-width: 769px)');
-
+  
   let testimonialSwiper;
-
+  
   if (window.innerWidth <= 768) {
     addStylesheet('https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css');
     addScript('https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', null, function () {
@@ -74,7 +80,7 @@ docReady(function () {
           enabled: true
         }
       });
-
+  
       const integrationSwiper = new Swiper('.integration-swiper', {
         spaceBetween: 100,
         centeredSlides: true,
@@ -82,7 +88,7 @@ docReady(function () {
         autoplay: {
           delay: 5000
         },
-
+  
         pagination: {
           el: '.swiper-pagination-integrations',
           clickable: true
@@ -93,14 +99,14 @@ docReady(function () {
       });
     });
   }
-
+  
   addScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.0/gsap.min.js', null, function () {
     const mobileMenuToggle = document.getElementById('mobileMenuToggle');
     const mobileMenuContent = document.getElementsByClassName('mobile-navbar-content')[0];
     const body = document.getElementsByTagName('body')[0];
-
+  
     var menuBar = gsap.timeline({paused: true});
-
+  
     menuBar.to(
       '.bar-1',
       0.5,
@@ -111,7 +117,7 @@ docReady(function () {
       },
       'start'
     );
-
+  
     menuBar.to(
       '.bar-2',
       0.5,
@@ -120,7 +126,7 @@ docReady(function () {
       },
       'start'
     );
-
+  
     menuBar.to(
       '.bar-3',
       0.5,
@@ -131,12 +137,12 @@ docReady(function () {
       },
       'start'
     );
-
+  
     menuBar.reverse();
-
+  
     mobileMenuToggle.addEventListener('click', function () {
       menuBar.reversed(!menuBar.reversed());
-
+  
       if (!mobileMenuContent.classList.contains('active')) {
         TweenLite.fromTo(mobileMenuContent, 0.3, {opacity: 0}, {opacity: 1, display: 'block'});
         mobileMenuContent.classList.add('active');
@@ -147,4 +153,4 @@ docReady(function () {
       body.classList.toggle('overflow-hidden');
     });
   });
-});
+}, {forPage: () => true});
